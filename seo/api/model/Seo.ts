8981c302593054ee011fce2ead60ec818499c593
Filip Chalupa <chalupa.filip@gmail.com>
2022-01-19 16:36:33 +0100

    Build

--- seo/api/model/Seo.ts
+++ seo/api/model/Seo.ts
@@ -1,10 +1,14 @@
+import { Model } from '@contember/schema'
 import { SchemaDefinition as d } from '@contember/schema-definition'
+import { Interface } from '@contember/schema-definition/dist/src/model/definition/types'
 import { Image } from '../../../image/api/model'
 
 export class Seo {
-	title = d.stringColumn()
-	description = d.stringColumn()
-	ogTitle = d.stringColumn()
-	ogDescription = d.stringColumn()
+	title: Interface<d.ColumnDefinition<Model.ColumnType>> = d.stringColumn()
+	description: Interface<d.ColumnDefinition<Model.ColumnType>> =
+		d.stringColumn()
+	ogTitle: Interface<d.ColumnDefinition<Model.ColumnType>> = d.stringColumn()
+	ogDescription: Interface<d.ColumnDefinition<Model.ColumnType>> =
+		d.stringColumn()
 	ogImage = d.oneHasOne(Image).cascadeOnDelete()
 }
