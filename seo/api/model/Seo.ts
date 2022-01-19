478db5efb6a277cae1f04b30b9a3257dd542d3ba
Filip Chalupa <chalupa.filip@gmail.com>
2022-01-19 17:28:14 +0100

    Fix types

--- seo/api/model/Seo.ts
+++ seo/api/model/Seo.ts
@@ -1,14 +1,10 @@
-import { Model } from '@contember/schema'
 import { SchemaDefinition as d } from '@contember/schema-definition'
-import { Interface } from '@contember/schema-definition/dist/src/model/definition/types'
 import { Image } from '../../../image/api/model'
 
 export class Seo {
-	title: Interface<d.ColumnDefinition<Model.ColumnType>> = d.stringColumn()
-	description: Interface<d.ColumnDefinition<Model.ColumnType>> =
-		d.stringColumn()
-	ogTitle: Interface<d.ColumnDefinition<Model.ColumnType>> = d.stringColumn()
-	ogDescription: Interface<d.ColumnDefinition<Model.ColumnType>> =
-		d.stringColumn()
+	title = d.stringColumn()
+	description = d.stringColumn()
+	ogTitle = d.stringColumn()
+	ogDescription = d.stringColumn()
 	ogImage = d.oneHasOne(Image).cascadeOnDelete()
 }
