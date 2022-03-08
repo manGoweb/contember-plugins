d12d75964d8483e0208654a70ce081bb99ac662f
Filip Chalupa <chalupa.filip@gmail.com>
2022-03-08 13:49:19 +0100

    LinkField: add allowDisconnect

--- src/link/admin/LinkField.tsx
+++ src/link/admin/LinkField.tsx
@@ -1,21 +1,55 @@
-import { Block, Component, DiscriminatedBlocks, HasOne, SelectField, TextField } from '@contember/admin'
+import {
+	ActionableBox,
+	Block,
+	Box,
+	Component,
+	DiscriminatedBlocks,
+	Field,
+	HasOne,
+	SelectField,
+	TextField,
+	useEntity,
+} from '@contember/admin'
 
 export interface LinkFieldProps {
 	field: string
+	label: string
+	titleField?: boolean
+	allowDisconnect?: boolean
 }
 
-export const LinkField = Component<LinkFieldProps>(props => {
-	return (
-		<HasOne field={props.field}>
-			<TextField field="title" label="Title" />
-			<DiscriminatedBlocks field="type" label="Type">
-				<Block discriminateBy="internal" label="Internal">
-					<SelectField field="internalLink" label="URL" options="Linkable.url" />
-				</Block>
-				<Block discriminateBy="external" label="External">
-					<TextField field="externalLink" label="URL" />
-				</Block>
-			</DiscriminatedBlocks>
+export const LinkField = Component<LinkFieldProps>(
+	({ field, label, titleField = true, allowDisconnect = false }) => {
+		const rootEntity = useEntity()
+
+		const insideContent = (
+			<HasOne field={field}>
+				{titleField && <TextField field="title" label="Title" />}
+				<DiscriminatedBlocks field="type" label="Type">
+					<Block discriminateBy="internal" label="Internal">
+						<SelectField field="internalLink" label="URL" options="Linkable.url" />
+					</Block>
+					<Block discriminateBy="external" label="External">
+						<TextField field="externalLink" label="URL" />
+					</Block>
+				</DiscriminatedBlocks>
+			</HasOne>
+		)
+
+		const content = label ? <Box heading={label}>{insideContent}</Box> : insideContent
+
+		if (allowDisconnect) {
+			return <ActionableBox onRemove={() => rootEntity.disconnectEntityAtField(field)}>{content}</ActionableBox>
+		}
+
+		return content
+	},
+	({ field, titleField = true }) => (
+		<HasOne field={field}>
+			<Field field="type" />
+			{titleField && <Field field="title" />}
+			<SelectField field="internalLink" label={undefined} options="Linkable.url" />
+			<Field field="externalLink" />
 		</HasOne>
-	)
-})
+	),
+)
