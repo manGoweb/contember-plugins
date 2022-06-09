ac9234034104f548a330695e299d08f64cf26fc5
Lukáš Chalupa <luchalupa@gmail.com>
2022-06-09 14:42:40 +0200

    fix options type

--- src/image/admin/buildSnappyimgUrl.ts
+++ src/image/admin/buildSnappyimgUrl.ts
@@ -1,8 +1,8 @@
-import Snappyimg from 'snappyimg'
+import Snappyimg, { Options } from 'snappyimg'
 
 let snappyimg: null | Snappyimg = null
 
-export function buildSnappyimgUrl(originalUrl: string, options?: Partial<Snappyimg.Options>) {
+export function buildSnappyimgUrl(originalUrl: string, options?: Partial<Options>) {
 	if (snappyimg === null) {
 		snappyimg = new Snappyimg('i27kxs55bq0k', '336c7a61727a7a73373634317671', Snappyimg.Stage.Serve)
 	}
